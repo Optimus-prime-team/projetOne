5c31a500228e171d31abbe2172d21c7b26f1a7e2
Supermomie <fakhredinedev3.0@gmail.com>
2020-05-25 16:05:09 +0200

    massive update on fill database

--- src/indeed/mongo_indeed.py
+++ src/indeed/mongo_indeed.py
@@ -8,8 +8,6 @@ FONCTIONS :
     get_connection() -> retourne un curseur sur la collection 'offres_indeed'
     save_offers (liste_offre) -> enregistre la liste d'offre scrappées (voir expl ligne 23) pas de retour
     load_offers () -> retourne un dataFrame de la collection 'offres_indeed'
-    delete_doublon(df_scrappe) -> retourne un dataframe des offres scrappées qui ne sont 
-                                  pas deja presente dans la collection 'offres_indeed'
 
 @author: MonOrdiPro
 """
@@ -17,21 +15,16 @@ FONCTIONS :
 import pymongo
 import pandas as pd
 from termcolor import colored
-#cols = ['entreprise', "poste", 'salaire', 'lieu', 'description']
 cols = ['adId', 'dataJk','city', 'contrat', 'salary','title', 'compagnyName', 
+
      'description', 'postdate', 'overOneMounth', 'job_querry', 'city_querry']
 
-databaseName   = "mydatabase1"
-collectionName = "offres_indeed1"
-
-
-def drop_collection() :
-    mycol = pymongo.MongoClient()[databaseName][collectionName]
-    mycol.drop()
+databaseName   = "indeed"
+collectionName = "offres_indeed"
 
 def get_connection() :
+    #client = pymongo.MongoClient("mongodb+srv://Fakhredine:4jM92%2Aqh%23fw3@cluster0-4chav.mongodb.net/test?retryWrites=true&w=majority")
     client = pymongo.MongoClient()
-
     dblist = client.list_database_names()
     if databaseName not in dblist:
         print('db created')
@@ -49,18 +42,35 @@ def save_offers (liste_offre) :
     
     df_scrappe = pd.DataFrame(liste_offre)
     df_scrappe = df_scrappe.infer_objects() # Convert automatically each dtypes for each columns's dataframe 
-    force_del, msg, df_to_add = delete_doublon(df_scrappe)
-    if len(df_to_add) != 0 :
-        ma_liste = df_to_add.to_dict('records')
-        mycol = get_connection()
-        x = mycol.insert_many(ma_liste)
-        print('ajout de : '+str(len(df_to_add))+ ' elements dans la collection offres_indeed')
-    else :
-        if force_del == True:
-            print(colored('ERROR :'+str(msg), 'red', attrs=["bold"]))
-        else:
-            print('Rien à ajouter !')
+    collection = get_connection()
+    a = df_scrappe.to_dict('records')[0]
+    findIfexist = collection.find_one({'adId' : a['adId']}, { })
+   
+    try:
+        if findIfexist != None:
+            mongoId = findIfexist['_id']
+            update = collection.find_one_and_update(
+                    {"_id": mongoId},
+                    {
+                    "$set": {
+                             "postdate"     : a['postdate'],
+                             "city_querry"  : a['city_querry'],
+                             "overOneMounth": a['overOneMounth']
+                             }
+                    },
+                )
+            update
+            return colored("update DONE", "yellow", attrs=["bold"])
 
+        else:
+            insert = collection.insert_one(a)
+            insert
+            return colored("insert Done", "blue", attrs=["bold"])
+
+    except:
+        return colored("NO","red", attrs=["bold"])
+
+    
 def load_offers ():
     mycol = get_connection()
     mylist = mycol.find()
@@ -70,35 +80,3 @@ def load_offers ():
         del df['_id']
     print('il y a actuellement : '+str(len(df))+' elements dans la collection')
     return df
-
-def delete_doublon(df_scrappe):
-    df_in_base = load_offers()
-    if len(df_in_base) == 0 :
-        
-        df_in_base = pd.DataFrame([['','','','',0.0,'','','','',0,'','']],columns=cols) #TODO rendre la creation du premier dataframe dynamic (dtypes) car si sur la toute premiere page de scrap aucun salaire n'est renseigner l'insertion en base de cette page ne se fera pas.
-    try:
-        #print(df_scrappe.dtypes)
-        #df_scrappe = df_scrappe.infer_objects()
-        comparaison_df = df_in_base.merge(df_scrappe,
-                                  indicator=True,
-                                  on=cols, #TODO LANCE LE SCRIPT TEL QUEL POUR VERIFIER LE REPLACEMENT DES DATES DURANT LE MERGE !!
-                                  how='right')
-        diff_df = comparaison_df[comparaison_df['_merge'] != 'both']
-        del diff_df['_merge']
-        return False, "", diff_df
-    except Exception as err:
-            return True, err, [] #TODO Corriger ICI car il skip les donnees de la page
-    
-    #return diff_df, force_d
-
-
-
-#drop_collection()
-
-x = load_offers()
-
-
-
-
-
-
