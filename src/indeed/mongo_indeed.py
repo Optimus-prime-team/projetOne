6088b5f2bc0d552732533a5a19c921c49a0574da
Supermomie <fakhredinedev3.0@gmail.com>
2020-05-27 19:03:59 +0200

    add all informations to be updated

--- src/indeed/mongo_indeed.py
+++ src/indeed/mongo_indeed.py
@@ -15,14 +15,12 @@ FONCTIONS :
 import pymongo
 import pandas as pd
 from termcolor import colored
-cols = ['adId', 'dataJk','city', 'contrat', 'salary','title', 'compagnyName', 
 
-     'description', 'postdate', 'overOneMounth', 'job_querry', 'city_querry']
+DATABASENAME   = "indeed_b"
+collection_indeed_offers  = "offres_indeed"
+collection_indeed_history = "offres_indeed_slave"
 
-databaseName   = "indeed"
-collectionName = "offres_indeed"
-
-def get_connection() :
+def get_connection(databaseName, collectionName) :
     #client = pymongo.MongoClient("mongodb+srv://Fakhredine:4jM92%2Aqh%23fw3@cluster0-4chav.mongodb.net/test?retryWrites=true&w=majority")
     client = pymongo.MongoClient()
     dblist = client.list_database_names()
@@ -42,29 +40,40 @@ def save_offers (liste_offre) :
     
     df_scrappe = pd.DataFrame(liste_offre)
     df_scrappe = df_scrappe.infer_objects() # Convert automatically each dtypes for each columns's dataframe 
-    collection = get_connection()
+    collection = get_connection(DATABASENAME, collection_indeed_offers)
+    collection1 = get_connection(DATABASENAME, collection_indeed_history)
     a = df_scrappe.to_dict('records')[0]
     findIfexist = collection.find_one({'adId' : a['adId']}, { })
    
     try:
         if findIfexist != None:
             mongoId = findIfexist['_id']
-            update = collection.find_one_and_update(
+            collection.find_one_and_update(
                     {"_id": mongoId},
                     {
                     "$set": {
+                             "adId"         : a['adId'],
+                             "dataJk"       : a['dataJk'],
+                             "compagnyName" : a['compagnyName'],
+                             "job_querry"   : a['job_querry'],
+                             "salary"       : a['salary'],
+                             "title"        : a['title'],
+                             "contrat"      : a['contrat'],
+                             "description"  : a['description'],
+                             "title"        : a['title'],
+                             "scrapDate"    : a['scrapDate'],
                              "postdate"     : a['postdate'],
                              "city_querry"  : a['city_querry'],
                              "overOneMounth": a['overOneMounth']
                              }
                     },
                 )
-            update
+            collection1.insert_one(a)
             return colored("update DONE", "yellow", attrs=["bold"])
 
         else:
-            insert = collection.insert_one(a)
-            insert
+            collection.insert_one(a)
+            collection1.insert_one(a)
             return colored("insert Done", "blue", attrs=["bold"])
 
     except:
@@ -72,7 +81,7 @@ def save_offers (liste_offre) :
 
     
 def load_offers ():
-    mycol = get_connection()
+    mycol = get_connection(DATABASENAME, collection_indeed_offers)
     mylist = mycol.find()
     df = pd.DataFrame(list(mylist))
     
@@ -80,3 +89,11 @@ def load_offers ():
         del df['_id']
     print('il y a actuellement : '+str(len(df))+' elements dans la collection')
     return df
+
+
+def history():
+    mycol = get_connection(DATABASENAME, collection_indeed_history)
+    mylist = mycol.find()
+
+    #TODO regarder l'history
+    print("")
