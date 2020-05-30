e46ee5bb55c9f1435c049d86eb42e3804093c7fe
Supermomie <fakhredinedev3.0@gmail.com>
2020-05-30 13:14:51 +0200

    massive update dash

--- src/dash/resume.py
+++ src/dash/resume.py
@@ -11,20 +11,29 @@ import dash_html_components as html
 import plotly.express as px
 import sys
 sys.path.append('../indeed/')
+sys.path.append('../preprocess/')
 import mongo_indeed as bdd
+import preprocess as prepros
 import numpy as np
+import pandas as pd
 colors ={
     'background': '#111111',
     'text': '#7FDBFF'
   }
 
-df =  bdd.load_offers()
+df = pd.read_csv('./indeed.csv')
+# df = prepros.merge_contrat()
+
+# print(df.columns)
+# exit()
+# df =  bdd.load_offers()
 
 # data Cleaning
 df.drop(index = df[df['city_querry'].isnull()].index, inplace = True)
 
+
 # convert 0.0 to nan
-df['salary'] = df['salary'].replace(0, np.nan)
+# df['salary'] = df['salary'].replace(0, np.nan)
 
 # mise en place du titre
 title ="Nombre d'offre scrappées : "+str(len(df))
@@ -59,9 +68,9 @@ data_job = [
 
 # données pour le pie % d'offre avec salaire
 labels_salaire = ['avec salaire', 'sans salaire']
-df_avec_salaire = df[df['salary'].isnull()==False]
+df_avec_salaire = df[df['salary'] > 0]
 
-nbr_salaire = [len(df[df['salary'].isnull()==False]), (len(df) - len(df[df['salary'].isnull()==False]))]
+nbr_salaire = [len(df_avec_salaire), (len(df) - len(df_avec_salaire))]
 
 data_salaire = [
         {
@@ -77,7 +86,11 @@ label_j = list(df_mean_salaire_par_cat.groups.keys())
 
 # print(label_j)
 df_mean_salaire_par_cat = df_mean_salaire_par_cat['salary'].mean().to_numpy().astype(int)
+df_max_salaire_par_cat = df_avec_salaire.groupby('job_querry')['salary'].max().to_numpy().astype(int)
+df_min_salaire_par_cat = df_avec_salaire.groupby('job_querry')['salary'].min().to_numpy().astype(int)
 
+# print(df_max_salaire_par_cat)
+# exit()
 # print(df_mean_salaire_par_cat)
 
 
@@ -87,15 +100,73 @@ data_salaire_par_metier = [
             'x': label_j, 
             'y': df_mean_salaire_par_cat, 
             'type': 'bar', 
-            'name': label_j
+            'name': 'moyenne'
         },
         {
-            'x': [1, 2, 3], #TODO finir ici sur le dataset reequilibré
-            'y': [2, 4, 5], 
+            'x': label_j, 
+            'y': df_max_salaire_par_cat, 
             'type': 'bar', 
-            'name': u'Montréal'
+            'name': 'max'
+        },
+        {
+            'x': label_j, 
+            'y': df_min_salaire_par_cat, 
+            'type': 'bar', 
+            'name': 'min'
         },
     ]
+
+
+
+
+# données pour le plot bar d'offres les mieux payer dans toute la base et base échantillonner
+df_mean_salaire_par_contrat = df[df['salary'] > 0].groupby('contrat')
+
+label_contrat = list(df_mean_salaire_par_contrat.groups.keys())
+
+# print(label_contrat)
+# exit()
+df_mean_salaire_par_contrat = df_mean_salaire_par_contrat['salary'].mean().to_numpy().astype(int)
+# print(df_mean_salaire_par_contrat)
+# exit()
+df_max_salaire_par_contrat = df_avec_salaire.groupby('contrat')['salary'].max().to_numpy().astype(int)
+df_min_salaire_par_contrat = df_avec_salaire.groupby('contrat')['salary'].min().to_numpy().astype(int)
+
+# print(df_min_salaire_par_contrat)
+# print(df_max_salaire_par_contrat)
+# exit()
+# print(df_mean_salaire_par_contrat)
+
+
+# exit()
+data_salaire_par_contrat = [
+        {
+            'x': label_contrat, 
+            'y': df_mean_salaire_par_contrat, 
+            'type': 'bar', 
+            'name': 'moyenne'
+        },
+        {
+            'x': label_contrat, 
+            'y': df_max_salaire_par_contrat, 
+            'type': 'bar', 
+            'name': 'max'
+        },
+        {
+            'x': label_contrat, 
+            'y': df_min_salaire_par_contrat, 
+            'type': 'bar', 
+            'name': 'min'
+        },
+    ]
+
+
+
+
+
+
+
+
 
 
 
@@ -167,13 +238,27 @@ def get_content():
                 figure={
                         'data': data_salaire_par_metier,
                         'layout': {
-                          'title': "salaire les mieux payer par metier",
+                          'title': "salaire par metier",
                           'paper_bgcolor': colors['background'],
                           'font': {
                               'color': colors['text']
                           }
                         },    
                     })
-            ], className='six columns')                
+            ], className='six columns'),
+            html.Div([
+                dcc.Graph(
+                id='bar_salary',
+                figure={
+                        'data': data_salaire_par_contrat,
+                        'layout': {
+                          'title': "salaire par contart",
+                          'paper_bgcolor': colors['background'],
+                          'font': {
+                              'color': colors['text']
+                          }
+                        },    
+                    })
+            ], className='six columns')                    
     ], className='row'),
   ])
